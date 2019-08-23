3819b4ed20e2b642167c806209ab3daae9828b53
Nguyen <dangtam16@gmail.com>
2019-08-23 00:53:09 -0400

    Upwards thrust/movement, needs tweaking

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrust");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left Rotation");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right Rotation");
+        }
+    }
+}
