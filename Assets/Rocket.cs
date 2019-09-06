52be5ab71988d6dbfad30df86cef97917bcd2e26
Nguyen <dangtam16@gmail.com>
2019-09-06 15:03:19 -0400

    Movement with audio added, minor tweaks to movement, noted that physics in Unity engine do not function properly with inspector open

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,33 +6,87 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    bool audioIsPlaying;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    void ProcessInput()
+    void PlayAudio(bool togglePlaying)
     {
+        if (audioIsPlaying == false && togglePlaying == true)
+        {
+            audioSource.Play();
+            audioIsPlaying = true;
+        }
+    }
+
+    void StopAudio()
+    {
+        audioSource.Stop();
+        audioIsPlaying = false;
+    }
+
+    void Thrust()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            print("Thrust");
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+            PlayAudio(true);
         }
+        else
+        {
+            StopAudio();
+        }
+    }
+
+    void Rotate()
+    {
+        rigidBody.angularVelocity = Vector3.zero;
+        rigidBody.freezeRotation = true; //Take manual control of rotation
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            print("Left Rotation");
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Right Rotation");
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //allow physics to control rotation now
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //Do nothing
+                print("Not dead");
+                break;
+            default:
+                //We die
+                print("REEEEEEE WE CRASHED");
+                break;
         }
     }
 }
